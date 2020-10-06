7731124d1444197b6b52fd09717f4e6618d421a0
prncher <prncher@gmail.com>
2020-10-06 11:13:55 -0700

    Initial commit of loanmodify

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const crypto = require('crypto');
+const config = require('./config.json');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+/* ----- session ----- */
+app.use(cookieSession({
+  name: 'session',
+  keys: [crypto.randomBytes(32).toString('hex')],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+app.use(cookieParser())
+
+/* ----- serve static ----- */
+app.use(express.static(path.join(__dirname, 'ui/build')));
+
+const port = config.port || 3000;
+app.listen(port);
+console.log(`Started app on port ${port}`);
+
+module.exports = app;
